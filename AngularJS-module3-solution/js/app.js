86467a5338b9747c7f3253401c62efe675b0c587
Ahmad <Ahmad.AlHalabi@outlook.com>
2021-01-03 22:52:40 +0200

    save

--- AngularJS-module3-solution/js/app.js
+++ AngularJS-module3-solution/js/app.js
@@ -17,12 +17,12 @@ function FoundItemsDirective() {
       onRemove: '&'
     },
     controller: FoundItemsDirectiveController,
-    controllerAs: 'list',
+    	: 'list',
     bindToController: true
   };
-
   return ddo;
 }
+
 
 function FoundItemsDirectiveController() {
   var list = this;
